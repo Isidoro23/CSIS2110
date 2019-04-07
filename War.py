93f425a64cb4d4b48fd13045ca98078ec2c1305b
Unknown <samisidoro@gmail.com>
2019-04-07 16:20:03 -0400

    Commenting done but still broken
    Commenting is done but the code still breaks when a list is empty. Not exiting while loop.

--- War.py
+++ War.py
@@ -44,13 +44,17 @@ class War:
         opponentTemp = []
         i = 0
 
+        # Continuously runs until the players deck is 0 or 52
         while(len(list1) != 0 or len(list1) != 52):
-            shuffle(list1)
-            shuffle(list2)
-            pCard = list1.pop()
-            oCard = list2.pop()
+            shuffle(list1) #Shuffles deck
+            shuffle(list2) #Shuffles deck
+            pCard = list1.pop() #Pops card out of players deck
+            oCard = list2.pop() #Pops card out of opponents deck
             print("Players card:" , + pCard)
             print("Opponents card:" , + oCard)
+            
+            # If the players card is greater than the opponents, It will add it to their deck
+            # and also add any cards from the equals temporary list
             if(pCard > oCard):
                 list1.append(oCard)
                 list1.append(pCard)
@@ -63,7 +67,9 @@ class War:
                     i = 0
                 print("Player wins!")
                 print("List 1 size: ", + len(list1), "\n")
-                    
+                
+            # If the opponents card is greater than the players, It will add it to their deck
+            # and also add any cards from the equals temporary list     
             elif(oCard > pCard):
                 list2.append(oCard)
                 list2.append(pCard)
@@ -76,19 +82,21 @@ class War:
                     i = 0
                 print("Opponent wins!")
                 print("List 2 size: ", + len(list2), "\n")
-                                        
+
+            # If both cards are equal, they are stored in a temporary list until someone wins                          
             elif(pCard == oCard):
                 playerTemp.append(pCard)
                 opponentTemp.append(oCard)
                 i = i + 1
                 print("Tie!\n")
 
+        # Prints Victory / Defeat
         if(len(list2) == 0):
             print("You have won the game of war!!")
         else:
             print("You have lost the game of war")
         
-
+    # Performs the method
     doWar(playerDeck, opponentDeck)
         
 
