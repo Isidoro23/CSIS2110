6ba602f7bf1e1b4f83028758fbeb8f5b5682d765
Unknown <samisidoro@gmail.com>
2019-04-07 16:01:37 -0400

    Nearly completed code with some comments

--- War.py
+++ War.py
@@ -3,11 +3,12 @@ from random import shuffle
 class War:
 
     
-
+    # Method used to create the deck
     def createDeck():
         totalDeck = []
         playerDeck = []
         opponentDeck = []
+        # Adds 4 sets of each card into the deck
         for x in range(4):
             totalDeck.append(1)
             totalDeck.append(2)
@@ -23,29 +24,69 @@ class War:
             totalDeck.append(12)
             totalDeck.append(13)
 
+        # Shuffles the deck 3 times
         for x in range(3):
             shuffle(totalDeck)
 
+        # Splits the deck into two
         while(len(totalDeck) != 0):
             playerDeck.append(totalDeck.pop())
             opponentDeck.append(totalDeck.pop())
 
         return playerDeck, opponentDeck;
 
+    # Returns decks outside of method
     playerDeck, opponentDeck = createDeck()
 
+    # Method used to play the game
     def doWar(list1, list2):
         playerTemp = []
         opponentTemp = []
+        i = 0
 
-        while(len(list1) != 0 or len(list2) != 0):
+        while(len(list1) != 0 or len(list1) != 52):
+            shuffle(list1)
+            shuffle(list2)
             pCard = list1.pop()
             oCard = list2.pop()
-            print(pCard)
-            print(oCard)
+            print("Players card:" , + pCard)
+            print("Opponents card:" , + oCard)
             if(pCard > oCard):
-                #Main Algorithm
-                pass
+                list1.append(oCard)
+                list1.append(pCard)
+                if(len(playerTemp) != 0 and len(opponentTemp) != 0):
+                    for i in range(len(playerTemp)):
+                        list1.append(playerTemp[i])
+                        list1.append(opponentTemp[i])
+                    playerTemp.clear()
+                    opponentTemp.clear()
+                    i = 0
+                print("Player wins!")
+                print("List 1 size: ", + len(list1), "\n")
+                    
+            elif(oCard > pCard):
+                list2.append(oCard)
+                list2.append(pCard)
+                if(len(playerTemp) != 0 and len(opponentTemp) != 0):
+                    for i in range(len(playerTemp)):
+                        list2.append(playerTemp[i])
+                        list2.append(opponentTemp[i])
+                    playerTemp.clear()
+                    opponentTemp.clear()
+                    i = 0
+                print("Opponent wins!")
+                print("List 2 size: ", + len(list2), "\n")
+                                        
+            elif(pCard == oCard):
+                playerTemp.append(pCard)
+                opponentTemp.append(oCard)
+                i = i + 1
+                print("Tie!\n")
+
+        if(len(list2) == 0):
+            print("You have won the game of war!!")
+        else:
+            print("You have lost the game of war")
         
 
     doWar(playerDeck, opponentDeck)
